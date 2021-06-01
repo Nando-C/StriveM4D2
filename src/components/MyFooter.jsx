8ccdb1f6d2bd66358fd134506ba7a345bc6e5b4a
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-06-01 14:53:42 +0100

    added NavBar and footer components
    Footer: placeholder for now

---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,11 @@
+
+
+const MyFooter = (props) => (
+    <div class=" bg-dark text-white">
+        <div className="container py-3">
+            <span class="text-muted">Here is where my footer will be placed</span>
+        </div>
+    </div>
+)
+
+export default MyFooter
